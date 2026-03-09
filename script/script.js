c97148fed301e2651384c6b2db03054c1f2649fb
mehedi <mehedi25122003@gmail.com>
2026-03-09 16:37:55 +0600

    dynamic cards done

--- script/script.js
+++ script/script.js
@@ -1,20 +1,69 @@
-const loadAllCardsData = () =>{
-    fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
-    .then(res=>res.json())
-    .then(data=>{
-        displayData(data.data);
-    })
-}
+const loadAllCardsData = () => {
+  fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then((res) => res.json())
+    .then((data) => {
+      displayData(data.data);
+    });
+};
 
 const displayData = (datas) => {
-    const dataViewer = document.getElementById('cardContainer');
-    dataViewer.innerHTML = ''
+  const dataViewer = document.getElementById("cardContainer");
+  dataViewer.innerHTML = "";
 
-    for(const data of datas){
-        const card = document.createElement('div');
-        card.innerHTML = `<div
+  for (const data of datas) {
+    const card = document.createElement("div");
+    // dynamic border
+    let borderCl = "border-[#00A96E]";
+    if (data.status === "closed") {
+      borderCl = "border-[#A855F7]";
+    }
+    // dynamic badge background
+    let badgeCl = "text-[#EF4444] bg-[#FEECEC]";
+    if (data.priority === "high") {
+      badgeCl = "text-[#EF4444] bg-[#FEECEC]";
+    } else if (data.priority === "medium") {
+      badgeCl = "text-[#F59E0B] bg-[#FFF6D1]";
+    } else if (data.priority === "low") {
+      badgeCl = "text-[#9CA3AF] bg-[#EEEFF2]";
+    }
+
+    // card labels
+let labelContainer = '';
+    data.labels.forEach(label=>{
+        let labelColor = "text-[#EF4444] bg-[#FEECEC]";
+        if(label === "bug") {
+            labelColor ='text-[#EF4444] bg-[#FEECEC] border border-[#FECACA]'
+        }
+        else if(label === "help wanted") {
+            labelColor ='bg-[#FFF8DB] text-[#D97706] border border-[#FECACA]'
+        }
+        else if(label === "enhancement") {
+            labelColor ='bg-[#DEFCE8] text-[#00A96E] border border-[#00A96E]'
+        }
+        else{
+            labelColor ='bg-[#FFF8DB] text-[#D97706] border border-[#FECACA]'
+        }
+
+        let icon = `<i class="fa-solid fa-wand-magic-sparkles"></i>`;
+        if(label === "bug") {
+            icon = `<i class="fa-solid fa-bug"></i>`;
+        } else if(label === "help wanted") {
+            icon = `<i class="fa-regular fa-life-ring"></i>`;
+        } else if(label === "enhancement") {
+            icon = `<i class="fa-solid fa-wand-magic-sparkles"></i>`;
+        }
+
+        labelContainer += `
+        <p
+                      class="uppercase text-[8px] ${labelColor} font-semibold  py-1.5 rounded-full px-4 "
+                    >
+                      ${icon} ${label}
+                    </p>
+        `
+    })
+    card.innerHTML = `<div
             onclick="my_modal_5.showModal()"
-              class="bg-white shadow rounded-lg border-t-2 border-[#A855F7] p-4"
+              class="bg-white shadow rounded-lg border-t-2 ${borderCl} p-4"
             >
               <div class="flex justify-between items-center mb-3">
                 <div class="">
@@ -26,14 +75,14 @@ const displayData = (datas) => {
                 </div>
                 <div>
                   <p
-                    class="uppercase text-xs text-[#EF4444] font-semibold bg-[#FEECEC] py-1.5 rounded-full px-4"
+                    class="uppercase text-xs ${badgeCl} font-semibold  py-1.5 rounded-full px-4"
                   >
                     ${data.priority}
                   </p>
                 </div>
               </div>
               <div>
-                <h1 class="font-semibold text-sm mb-1">
+                <h1 class="font-semibold text-sm mb-1 line-clamp-1">
                   ${data.title}
                 </h1>
                 <p class="text-[#64748B] text-xs line-clamp-2">
@@ -41,28 +90,18 @@ const displayData = (datas) => {
                 </p>
 
                 <div class="mt-3">
-                  <div class="flex gap-1 items-center">
-                    <p
-                      class="uppercase text-xs text-[#EF4444] font-semibold bg-[#FEECEC] py-1.5 rounded-full px-4 border border-[#FECACA]"
-                    >
-                      <i class="fa-solid fa-bug"></i> bug
-                    </p>
-
-                    <p
-                      class="uppercase text-xs font-semibold bg-[#FFF8DB] text-[#D97706] py-1.5 rounded-full px-4 border border-[#FDE68A]"
-                    >
-                      <i class="fa-regular fa-life-ring"></i> help wanted
-                    </p>
+                  <div id="cardLabels" class="flex flex-wrap gap-1 items-center">
+                    ${labelContainer}
                   </div>
                 </div>
                 <div class="divider -mx-4"></div>
                 <div class="space-y-2">
-                  <p class="text-xs text-[#64748B]">#${data.assignee ? data.assignee : 'Unassigned'}</p>
+                  <p class="text-xs text-[#64748B]">#by ${data.assignee ? data.assignee : "Unassigned"}</p>
                   <p class="text-xs text-[#64748B]">${data.createdAt}</p>
                 </div>
               </div>
-            </div>`
-        dataViewer.appendChild(card);
-    }
-}
-loadAllCardsData()
+            </div>`;
+    dataViewer.appendChild(card);
+  }
+};
+loadAllCardsData();
